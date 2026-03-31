377ed8cfe4e3337f157ef9560460de6b466fcc9a
cfjapan47-hash <cfjapan47@gmail.com>
2026-03-31 16:06:33 +0900

    Implement logout handler to clear authentication cookie

--- api/auth-logout.js
+++ api/auth-logout.js
@@ -1,1 +1,7 @@
+module.exports = async function handler(req, res) {
+  res.setHeader('Set-Cookie',
+    'salon-auth=; HttpOnly; Path=/; Max-Age=0; SameSite=Strict'
+  );
+  return res.status(200).json({ success: true });
+};
 
